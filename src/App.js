58f51248d7fd69620897b4b0e838da5bd11eadc1
Isha <isha.20hcs4125@ddu.du.ac.in>
2022-09-11 17:22:08 +0530

    react-router-dom changed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Routes } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import Contact from './components/Contact';
 import Home from './components/Home';
 import './components/style.scss';
@@ -8,10 +8,14 @@ import 'rsuite/dist/rsuite.min.css';
 function App() {
   return (
     <div className='main'>
-    <Routes>
-        <Route exact path='/' element={<Home />} />
-        <Route path='/contact' element={<Contact />} />
-    </Routes>
+      <Switch>
+        <Route exact path='/'>
+          <Home />
+        </Route>
+        <Route path='/contact'>
+          <Contact />
+        </Route>
+      </Switch>
     </div>
   );
 }
